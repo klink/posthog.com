8b63bb4a664af59e2222bfed9fea0749777661c8
Paul Hultgren <pjhultgren5@gmail.com>
2022-07-14 16:30:14 -0700

    use flex layout instead of grid to fix #3764

--- src/components/PostLayout/index.js
+++ src/components/PostLayout/index.js
@@ -166,16 +166,16 @@ export default function PostLayout({
 
     return (
         <div
-            style={{ gridAutoColumns: `1fr minmax(auto, ${contentWidth}px) minmax(max-content, 1fr)` }}
-            className="w-full relative lg:grid lg:grid-flow-col items-start -mb-20"
+            style={{ gridTemplateColumns: `` }}
+            className="w-full relative flex flex-col items-stretch lg:flex-row lg:items-start lg:justify-end"
         >
-            <article className="col-span-2 px-5 lg:px-8 lg:border-r border-dashed border-gray-accent-light dark:border-gray-accent-dark mt-10 lg:mt-0 lg:pt-10 lg:pb-20 ml-auto">
+            <article className="flex-grow flex flex-col items-start lg:items-end px-5 lg:px-8 lg:border-r border-dashed border-gray-accent-light dark:border-gray-accent-dark mt-10 lg:mt-0 lg:pt-10 lg:pb-20">
                 <div style={{ maxWidth: contentWidth }} className={`w-full ${article ? 'article-content' : ''}`}>
                     {children}
                 </div>
                 {questions && questions}
             </article>
-            <aside className="lg:sticky top-10 flex-shrink-0 w-full justify-self-end lg:box-content my-10 lg:my-0 lg:mt-10 pb-20 mr-auto overflow-y-auto lg:h-[calc(100vh-7.5rem)]">
+            <aside className="lg:sticky top-10 flex-grow lg:max-w-[625px] my-10 lg:my-0 lg:mt-10 pb-20 overflow-y-auto lg:h-[calc(100vh-7.5rem)]">
                 <div className="grid divide-y divide-gray-accent-light dark:divide-gray-accent-dark divide-dashed">
                     {sidebar && sidebar}
                     {view === 'Article' && !breakpoints.md && toc?.length > 1 && (
